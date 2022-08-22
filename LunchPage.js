3cd474ef9d73d0d4a7bb9ee120b5149e565811ba
dainius.kaucikas <dainius.kaucikas@ktu.edu>
2022-08-22 16:05:18 +0300

    Automation updated

--- LunchPage.js
+++ LunchPage.js
@@ -5,24 +5,33 @@ class LunchPage {
         this.page = page;
     }
 
+    // Navigates to wanted page
     async navigate() {
         await this.page.goto('https://lunch.devbstaging.com/login-password');
     }
 
+    // Logins into the specified account
     async login(email, password) {
         await this.page.locator(`input[name='email']`).type(email);
         await this.page.locator(`input[name='password']`).type(password);
         await this.page.locator('.v-btn').click();
     }
 
-    async selectFirstSupplier() {
+    // Selects the first item to order from the list
+    async selectFirstItem() {
         await this.page.locator('.v-list__group__header').first().click();
         await this.page.locator('.v-list__group__header--active .v-list__tile').first().click();
-        await expect(this.page.locator('h3.red--text')).toHaveText('Todžė');
+        await this.page.locator('.v-card--hover').first().click();
     }
 
+    // Asserts whether the textToContain exists at given locator
     async assertText(locator, textToContain) {
         await expect(this.page.locator(locator)).toContainText(textToContain);
+    }
+
+    // Asserts whether an item exists in the cart
+    async assertFirstItemInCart() {
+        await expect(this.page.locator('.v-chip__content')).not.toBeEmpty();
     }
 }
 
--- LunchPageTests.spec.js
+++ LunchPageTests.spec.js
@@ -2,21 +2,17 @@
 const { test, expect } = require('@playwright/test');
 const { LunchPage } = require('./LunchPage');
 
+const loginEmail = 'Dainius.Kaucikas@sourceryacademy.com';
+const loginPassword = 'nera_svarbus47';
+
 test.describe('Tests', () => {
-  // test('Test description', async ({ page }) => {
-  //   const lunchPage = new LunchPage(page);
-
-  //   await lunchPage.navigate();
-  //   await lunchPage.login('Dainius.Kaucikas@sourceryacademy.com', 'nera_svarbus47');
-
-  //   await lunchPage.assertText('.v-subheader', 'Dainius Kaucikas');
-  // })
-
-  test('First supplier', async ({ page }) => {
+  test('Ordered item should appear in the navigation bar', async ({ page }) => {
     const lunchPage = new LunchPage(page);
 
     await lunchPage.navigate();
-    await lunchPage.login('Dainius.Kaucikas@sourceryacademy.com', 'nera_svarbus47');
-    await lunchPage.selectFirstSupplier();
+    await lunchPage.login(loginEmail, loginPassword);
+    await lunchPage.selectFirstItem();
+
+    await lunchPage.assertFirstItemInCart();
   })
 });
